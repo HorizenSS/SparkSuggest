c67928547d4aeeee60d13c4b4789b3fdfd3a821e
Ines Akez <ines.akez@esprit.tn>
2021-05-19 03:21:55 +0100

    final steps

--- src/main/java/com/rapid/engine/SwaggerConfig.java
+++ src/main/java/com/rapid/engine/SwaggerConfig.java
@@ -31,23 +31,8 @@ public class SwaggerConfig {
 	ApiInfo apiInfo = new ApiInfo(
 			"Autocomplete Engine RESTFUL Web Service documentation",
 
-					"To use the client side, start by feeding suggestions to the app." +
-							"You can use the testing data below to perform POST request using Postman." +
-							"Then head to the client side to test it out!\n" +
-							"" +
-							"[\n" +
-							"    {\n" +
-							"        \"target\": \"https://en.wikipedia.org/wiki/The_Matrix\",\n" +
-							"        \"suggestion\": \"the matrix 1\",\n" +
-							"        \"group\": \"movies\"\n" +
-							"    },\n" +
-							"    {\n" +
-							"        \"target\": \"https://en.wikipedia.org/wiki/The_Matrix_Reloaded\",\n" +
-							"        \"suggestion\": \"the matrix 2\",\n" +
-							"        \"group\": \"movies\"\n" +
-							"    }\n" +
-							"]" +
-							"or run the following curl command on https://reqbin.com/curl" +
+					"To use the client side, start by feeding suggestions to the app.\n" +
+							"Run the following curl command on https://reqbin.com/curl\n" +
 							"curl --location --request POST 'https://rapid-search-engine.herokuapp.com/api/post' --header 'Content-Type: application/json' --data-raw '[\n" +
 							"    {\n" +
 							"        \"target\": \"https://en.wikipedia.org/wiki/The_Matrix\",\n" +
@@ -59,7 +44,8 @@ public class SwaggerConfig {
 							"        \"suggestion\": \"the matrix 2\",\n" +
 							"        \"group\": \"movies\"\n" +
 							"    }\n" +
-							"]'",
+							"]'\n" +
+							"Then head to the client side to test it out!\n" +,
 			"1.0",
 			"https://github.com/HorizenSS/SparkSuggest",
 			contact, 
